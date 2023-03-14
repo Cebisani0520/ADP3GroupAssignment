7ca8323761839d869046c6afb5f480688a4a8f3e
Haydn-Abrahams <abrahamshaydn@gmail.com>
2023-03-14 17:03:42 +0200

    Student Class Added. StudentTest Added.

---
+++ src/test/java/za/ca/cput/domain/StudentTest.java
@@ -0,0 +1,12 @@
+package za.ca.cput.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+    @Test
+    public void test() {
+
+    }
+}
