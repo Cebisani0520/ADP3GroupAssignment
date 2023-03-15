83f17de825c83bc3c7abaf348e1a4b21670e1fe8
andreajacobs00 <jacobsandrea02@gmail.com>
2023-03-15 12:48:47 +0200

    This commit test object equality

---
+++ src/test/java/za/ac/cput/domain/StudentTest.java
@@ -0,0 +1,24 @@
+/** StudentTest.java
+ * This program runs test that explains object equality, object identity, disabling tests, failing test and timeouts.
+ * Andrea Jacobs 218024266
+ * 15 March 2023 */
+
+package za.ac.cput.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentTest {
+    public void testEquality() {
+        ArrayList<Student> list = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            Student s = new Student("Andrea", "Jacobs");
+            list.add(s);
+        }
+        assertEquals(10, list.size());
+    }
+  
+}
